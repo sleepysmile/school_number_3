3d20a665a6468b468a1e5c57e3a0dd38e9626a73
Roman <r.ostrovskiy@digital-sector.ru>
2019-04-07 16:23:00 +0300

    initl commit

---
+++ migrations/m190407_122538_create_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m190407_122538_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string()->comment('Имя пользователя'),
+            'password' => $this->string()->comment('passwords user'),
+            'email' => $this->string()->comment('E-mail users'),
+            'status' => $this->integer()->comment('status users'),
+            'created_at' => $this->string(),
+            'updated_at' => $this->string(),
+            'access_token' => $this->string(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
---
+++ models/User.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace app\models;
+
+use app\models\query\UserQuery;
+use Yii;
+use yii\base\BaseObject;
+use yii\behaviors\AttributeBehavior;
+use yii\behaviors\TimestampBehavior;
+use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
+use yii\web\IdentityInterface;
+
+class User extends ActiveRecord implements IdentityInterface
+{
+    const STATUS_NOT_ACTIVE = 1;
+    const STATUS_ACTIVE = 2;
+    const STATUS_DELETED = 3;
+
+
+    public $id;
+    public $username;
+    public $password;
+    public $authKey;
+    public $accessToken;
+
+    private static $users = [
+        '100' => [
+            'id' => '100',
+            'username' => 'admin',
+            'password' => 'admin',
+            'authKey' => 'test100key',
+            'accessToken' => '100-token',
+        ],
+        '101' => [
+            'id' => '101',
+            'username' => 'demo',
+            'password' => 'demo',
+            'authKey' => 'test101key',
+            'accessToken' => '101-token',
+        ],
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%user}}';
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentity($id)
+    {
+        return static::find()
+        ->active()
+        ->andWhere(['id' => $id])
+        ->one();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return static::find()
+            ->active()
+            ->andWhere(['access_token' => $token])
+            ->one();
+    }
+
+    /**
+     * Finds user by username
+     *
+     * @param string $username
+     * @return static|null
+     */
+    public static function findByUsername($username)
+    {
+        return static::find()
+            ->active()
+            ->andWhere(['username' => $username])
+            ->one();
+    }
+
+    public static function findByLogin($login)
+    {
+        return static::find()
+            ->active()
+            ->andWhere(['or', ['username' => $login], ['email' => $login]])
+            ->one();
+    }
+
+    public function behaviors()
+    {
+        return [
+            TimestampBehavior::class,
+            'auth_key' => [
+                'class' => AttributeBehavior::class,
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => 'auth_key'
+                ],
+                'value' => Yii::$app->getSecurity()->generateRandomString()
+            ],
+            'access_token' => [
+                'class' => AttributeBehavior::class,
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => 'access_token'
+                ],
+                'value' => function () {
+                    return Yii::$app->getSecurity()->generateRandomString(40);
+                }
+            ]
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function scenarios()
+    {
+        return ArrayHelper::merge(
+            parent::scenarios(),
+            [
+                'oauth_create' => [
+                    'oauth_client', 'oauth_client_user_id', 'email', 'username', '!status'
+                ]
+            ]
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'email'], 'unique'],
+            ['status', 'default', 'value' => self::STATUS_NOT_ACTIVE],
+            ['status', 'in', 'range' => array_keys(self::statuses())],
+            [['username'], 'filter', 'filter' => '\yii\helpers\Html::encode']
+        ];
+    }
+
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthKey()
+    {
+        return $this->authKey;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateAuthKey($authKey)
+    {
+        return $this->authKey === $authKey;
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return $this->password === $password;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return
+     */
+    public static function find()
+    {
+        return new UserQuery(static::class);
+    }
+
+}
